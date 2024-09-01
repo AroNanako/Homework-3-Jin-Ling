6ffef619f100c7c06897fa4f7331bcefad26feb6
AroNanako <ljin93773@gmail.com>
2024-09-02 00:36:04 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+pip install flask
+
+from flask import Flask,render_template,request
+import google.generativeai as palm
+
+api = "AIzaSyCaWXSq-kOenXykQ_Qu3cG6AHO-xlvGk4U"
+palm.configure(api_key=api)
+model = {"model": "models/chat-bison-001"}
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("DBS.html"))
+
+if __name__ == "__main__":
+    app.run()
